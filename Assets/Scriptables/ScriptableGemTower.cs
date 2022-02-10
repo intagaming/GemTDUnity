e5407bead57955ff40edf12798b7a10330c5a4d1
An Xuan Hoang <xuanan2001@gmail.com>
2022-02-11 00:31:17 +0700

    gem tower scriptables

--- Assets/Scriptables/ScriptableGemTower.cs
+++ Assets/Scriptables/ScriptableGemTower.cs
@@ -7,9 +7,6 @@ public class ScriptableGemTower : ScriptableTower
 {
   public GemTower towerPrefab;
 
-  // General info about the gem
-  public int[] levelDamages;
-
   // Blueprint for each individual gem instance
   public int gemLevel = 0;
 }
--- Assets/Scriptables/ScriptableTower.cs
+++ Assets/Scriptables/ScriptableTower.cs
@@ -1,7 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class ScriptableTower : ScriptableObject
 {
+  [SerializeField]
+  private TowerStats stats;
+  public TowerStats BaseStats => stats;
+}
+
+[Serializable]
+public struct TowerStats
+{
+  public int damage;
+  public float range;
 }
